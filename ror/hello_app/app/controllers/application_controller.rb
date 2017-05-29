fd69388571129121111587fc894031e5f7fc70c2
Radu Arsici <radu.arsici@gmail.com>
2017-05-29 11:27:19 -0700

    my files

--- ror/hello_app/app/controllers/application_controller.rb
+++ ror/hello_app/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "<p>This is my new App</p><p><a href='http://localhost:3000/users'>These are my users</a></p>".html_safe
   end
 end
