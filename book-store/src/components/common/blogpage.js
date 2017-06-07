93963787dc91c6456af2762f2a3d258092429f43
Radu Arsici <radu.arsici@gmail.com>
2017-06-06 19:44:15 -0700

    Blog page works ! No Css though

--- book-store/src/components/common/blogpage.js
+++ book-store/src/components/common/blogpage.js
@@ -1,10 +1,73 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as blogActions from '../../actions/blogActions';
 
 class Blogpage extends React.Component{
 
+	constructor(props){
+		super(props);
+	}
+
+
+	submitPost(content,author,title){
+		this.props.createBlog(content,author,title);
+		console.log(this.props.blog);
+	}
+
 	render(){
-		return <h1>Blog yay!</h1>
+		let titleInput, authorInput, contentInput;
+
+		return (
+			<div>
+				<h3>Blog Page</h3>
+					{this.props.blogs.map((b,i) => 
+						<b><p key={i}>{b.title.title}</p></b>)}
+					{this.props.blogs.map((b,i) => 
+						<p key={i}>{b.content.content}</p>)}
+					{this.props.blogs.map((b,i) => 
+						<p key={i}> Written by {b.author.author}</p>)}
+				
+				<div>
+				<h3>Books Form</h3>
+				<form onSubmit={e => {
+					e.preventDefault();
+
+					var tInput = {title: titleInput.value};
+					var aInput = {author: authorInput.value};
+					var cInput = {content: contentInput.value};
+
+					this.submitPost(cInput,aInput,tInput);
+
+
+					e.target.reset();
+				}}>
+					<h5>Title</h5>
+					<input type="text" name="title" ref={node => titleInput = node}/>
+					<h5>Author</h5>
+					<input type="text" name="author" ref={node => authorInput = node}/>
+					<h5>Content</h5>
+					<input type="text" name="content" ref={node => contentInput = node}/>
+					<h5></h5>
+            		<input type="submit" />
+          		</form>
+
+				</div>
+			</div>
+		)
 	}
 };
 
-export default Blogpage;
+const mapStateToProps = (state,ownProps) =>{
+	return {
+		blogs: state.blogs
+	}
+};
+
+const mapDispatchToProps = (dispatch) =>{
+	return {
+		createBlog: (content,author,title) => dispatch(blogActions.createBlog(content,author,title))
+	}
+
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Blogpage);
--- book-store/src/reducers/blogReducers.js
+++ book-store/src/reducers/blogReducers.js
@@ -3,7 +3,7 @@ export default (state = [], action) => {
 	switch(action.type){
 
 		case 'CREATE_BLOG': return [
-		...state, Object.assign({}, {action.content, action.author , action.title} );
+		...state, Object.assign({}, {content: action.content, author: action.author, title: action.title})
 		];
 
 		default: return state;
