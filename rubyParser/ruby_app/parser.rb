79e967c076e0689625b784fd4946691087148f3b
radood <radu.arsici@gmail.com>
2019-08-19 18:42:34 +0100

    added the completely wrong file to git

--- rubyParser/ruby_app/parser.rb
+++ rubyParser/ruby_app/parser.rb
@@ -1,37 +1,36 @@
 
-file = ""
-ARGV.each do|a|
-  file = File.read(a)
-end
+require './parserLibrary'
 
-singleArrayWith1Element = file.split("\n")
-singlearrayWith2Elements = singleArrayWith1Element.map{|i|i.split(' ')}
-finalHash = {}
-singlearrayWith2Elements.each do |i,j|
-	j = [j]
-	if(finalHash[i])
-		finalHash[i] = finalHash[i] + j
-	else
-		finalHash[i] = j
+class Parser
+
+	def runProgram
+		lib = ParserLibrary.new;
+
+		hashmap = lib.convertLogFileToHashMap(grabLogs)
+
+		lib.printNumberOfViewsInOrder(lib.countNumberOfViews(hashmap))
+
+		lib.printNumberOfUniqueViewsInOrder(lib.countNumberOfUniqueViews(hashmap))
 	end
+
+	def grabLogs
+		file = ""
+		ARGV.each do|a| 
+			file = File.read(a)
+		end
+		return file
+	end
+
+	private:grabLogs
 end
 
-numberOfViews = {}
-finalHash.each do |i,j|
-	numberOfViews[i] = j.count
-end
 
-numberOfViews = numberOfViews.sort_by{|a,b| b}.reverse
-numberOfViews.each do |i,j|
-	puts "For the #{i} page there are #{j} views."
-end
+p = Parser.new
+p.runProgram
 
-numberOfUniqueViews = {}
-finalHash.each do |i,j|
-	numberOfUniqueViews[i] = j.uniq.count
-end
 
-numberOfUniqueViews = numberOfUniqueViews.sort_by{|a,b| b}.reverse
-numberOfUniqueViews.each do |i,j|
-	puts "For the #{i} page there are #{j} unique views."
-end
+
+
+
+
+
